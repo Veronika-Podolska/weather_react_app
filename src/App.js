7a70403aeab77d79cf4ac056d91bd6358c5a5804
Veronika-Podolska <nixxxxx17@gmail.com>
2023-04-16 19:25:43 -0400

    fetching data function added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 export default function App() {
+  const [temperature, setTemperature] = useState(null);
+  const city = "Paris";
+  const apiKey = "b7btoe10de74d38af299c8c0fa0d3f98";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  function displayWeatherData(response) {
+    setTemperature(response.data.temperature.current);
+  }
+
+  axios.get(apiUrl).then(displayWeatherData);
+
   return (
     <div className="App">
       <form>
@@ -11,10 +23,12 @@ export default function App() {
       <div className="row">
         <div className="col">
           <h1>New York</h1>
-          <h2> Tuesday 15:07</h2>
+          <h2>Tuesday 15:07</h2>
           <h3>Clear Sky</h3>
           <div>
-            <span className="temperature-degree">27</span>{" "}
+            <span className="temperature-degree">
+              {Math.round(temperature)}
+            </span>{" "}
             <span className="celsius-link">°C</span>
             <img
               src="https://www.getillustrations.com/packs/zima-3d-illustrations-for-websites-and-apps/elements/_1x/weather%20_%20cloudy,%20partly,%20forecast,%20sunny,%20season,%20sun,%20day_md.png"
