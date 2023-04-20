5d65c6d91441715421c291a4efce2a81d6261c7b
Veronika-Podolska <nixxxxx17@gmail.com>
2023-04-19 22:06:33 -0400

    ajax call made

--- src/App.js
+++ src/App.js
@@ -3,56 +3,71 @@ import "./App.css";
 import axios from "axios";
 
 export default function App() {
-  const [temperature, setTemperature] = useState(null);
-  const city = "Paris";
-  const apiKey = "b7btoe10de74d38af299c8c0fa0d3f98";
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  const [ready, setReady] = useState(false);
 
-  function displayWeatherData(response) {
-    setTemperature(response.data.temperature.current);
+  const [weatherInfo, setWeatherInfo] = useState({});
+
+  function displayWeather(response) {
+    console.log(response);
+    setReady(true);
+
+    setWeatherInfo({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      pressure: response.data.temperature.pressure,
+      feelsLike: response.data.temperature.feels_like,
+      description: response.data.condition.description,
+      icon: "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png",
+      date: "Tuesaday 15:31",
+      city: response.data.city,
+    });
   }
 
-  axios.get(apiUrl).then(displayWeatherData);
-
-  return (
-    <div className="App">
-      <form>
-        <input type="search" placeholder="Enter the city" />
-        <input type="sumbit" value="search" />
-      </form>
-      <div className="row">
-        <div className="col">
-          <h1>New York</h1>
-          <h2>Tuesday 15:07</h2>
-          <h3>Clear Sky</h3>
-          <div>
-            <span className="temperature-degree">
-              {Math.round(temperature)}
-            </span>{" "}
-            <span className="celsius-link">°C</span>
-            <img
-              src="https://www.getillustrations.com/packs/zima-3d-illustrations-for-websites-and-apps/elements/_1x/weather%20_%20cloudy,%20partly,%20forecast,%20sunny,%20season,%20sun,%20day_md.png"
-              alt="sun-icon"
-            />
-          </div>
-        </div>
-        <div className="col">
-          <div className="row weather-info">
-            <div className="col ">
-              <ul>
-                <li>Wind</li>
-                <li>Sunset</li>
-              </ul>
+  if (ready) {
+    return (
+      <div className="App">
+        <form>
+          <input type="search" placeholder="Enter the city" />
+          <input type="sumbit" value="search" />
+        </form>
+        <div className="row">
+          <div className="col">
+            <h1>{weatherInfo.city}</h1>
+            <h2>{weatherInfo.date}</h2>
+            <h3>{weatherInfo.description}</h3>
+            <div>
+              <span className="temperature-degree">
+                {Math.round(weatherInfo.temperature)}
+              </span>{" "}
+              <span className="celsius-link">°C</span>
+              <img src={weatherInfo.icon} alt="sun-icon" />
             </div>
-            <div className="col">
-              <ul>
-                <li>Percipitation</li>
-                <li>Sunrise</li>
-              </ul>
+          </div>
+          <div className="col">
+            <div className="row weather-info">
+              <div className="col ">
+                <ul>
+                  <li>Wind: {Math.round(weatherInfo.wind)}km/h</li>
+                  <li>Feels like: {Math.round(weatherInfo.feelsLike)}°C</li>
+                </ul>
+              </div>
+              <div className="col">
+                <ul>
+                  <li>Humidity: {Math.round(weatherInfo.humidity)} %</li>
+                  <li>Pressure: {Math.round(weatherInfo.pressure)} mb </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const city = "Madrid";
+    const apiKey = "b7btoe10de74d38af299c8c0fa0d3f98";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+    return <div>Loading</div>;
+  }
 }
