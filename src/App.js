db3dd2d2a186079383c8649833835633ddf74bc8
Veronika-Podolska <nixxxxx17@gmail.com>
2023-04-25 21:22:30 -0400

    component w weather info added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import UpdatedDate from "./UpdatedDate";
+
+import WeatherInfo from "./WeatherInfo";
 
 export default function App() {
   const [ready, setReady] = useState(false);
@@ -31,40 +32,11 @@ export default function App() {
           <input type="search" placeholder="Enter the city" />
           <input type="sumbit" value="search" />
         </form>
-        <div className="row">
-          <div className="col">
-            <h1>{weatherInfo.city}</h1>
-            <UpdatedDate date={weatherInfo.date} />
-            <h3 className="text-capitalize">{weatherInfo.description}</h3>
-            <div>
-              <span className="temperature-degree">
-                {Math.round(weatherInfo.temperature)}
-              </span>{" "}
-              <span className="celsius-link">°C</span>
-              <img src={weatherInfo.icon} alt="sun-icon" />
-            </div>
-          </div>
-          <div className="col">
-            <div className="row weather-info">
-              <div className="col ">
-                <ul>
-                  <li>Wind: {Math.round(weatherInfo.wind)}km/h</li>
-                  <li>Feels like: {Math.round(weatherInfo.feelsLike)}°C</li>
-                </ul>
-              </div>
-              <div className="col">
-                <ul>
-                  <li>Humidity: {Math.round(weatherInfo.humidity)} %</li>
-                  <li>Pressure: {Math.round(weatherInfo.pressure)} mb </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherInfo} />
       </div>
     );
   } else {
-    const city = "Madrid";
+    const city = "Paris";
     const apiKey = "b7btoe10de74d38af299c8c0fa0d3f98";
     const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
---
+++ src/WeatherInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import UpdatedDate from "./UpdatedDate";
+
+export default function WeatherInfo({ data }) {
+  return (
+    <div className="weatherInfo">
+      <div className="row">
+        <div className="col">
+          <h1>{data.city}</h1>
+          <UpdatedDate date={data.date} />
+          <h3 className="text-capitalize">{data.description}</h3>
+          <div>
+            <span className="temperature-degree">
+              {Math.round(data.temperature)}
+            </span>{" "}
+            <span className="celsius-link">°C</span>
+            <img src={data.icon} alt="sun-icon" />
+          </div>
+        </div>
+        <div className="col">
+          <div className="row weather-info">
+            <div className="col ">
+              <ul>
+                <li>Wind: {Math.round(data.wind)}km/h</li>
+                <li>Feels like: {Math.round(data.feelsLike)}°C</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul>
+                <li>Humidity: {Math.round(data.humidity)} %</li>
+                <li>Pressure: {Math.round(data.pressure)} mb </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
