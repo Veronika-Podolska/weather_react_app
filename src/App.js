6500d3109429d07af381ffb6e30bb3e832e89d1f
Veronika-Podolska <nixxxxx17@gmail.com>
2023-04-26 00:15:05 -0400

    search engine integrated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-
 import WeatherInfo from "./WeatherInfo";
 
 export default function App() {
+  const defaultCity = "Kyiv";
   const [ready, setReady] = useState(false);
-
   const [weatherInfo, setWeatherInfo] = useState({});
+
+  const [city, setCity] = useState(defaultCity);
 
   function displayWeather(response) {
     setReady(true);
-
     setWeatherInfo({
       temperature: response.data.temperature.current,
       wind: response.data.wind.speed,
@@ -25,21 +25,38 @@ export default function App() {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleChangeCity(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "b7btoe10de74d38af299c8c0fa0d3f98";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
   if (ready) {
     return (
       <div className="App">
-        <form>
-          <input type="search" placeholder="Enter the city" />
-          <input type="sumbit" value="search" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter the city"
+            className="form-control"
+            onChange={handleChangeCity}
+          />
+          <button className="btn">search</button>
         </form>
         <WeatherInfo data={weatherInfo} />
       </div>
     );
   } else {
-    const city = "Paris";
-    const apiKey = "b7btoe10de74d38af299c8c0fa0d3f98";
-    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    search();
     return <div>Loading</div>;
   }
 }
